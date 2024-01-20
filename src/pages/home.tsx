75e371852c2415ccf9a11e4fd2d8d1018837323f
0xzion <0xzion.penx@gmail.com>
2024-01-20 23:51:40 +0800

    feat: improve home ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,14 +2,20 @@ import { Box } from '@fower/react'
 
 export const PageHome = () => {
   return (
-    <Box>
-      <Box as="h1">PenX Agent</Box>
-      <Box>PenX Agent is running on: http://localhost:65432</Box>
+    <Box textCenter>
+      <Box text3XL fontBold mb2>
+        PenX Agent
+      </Box>
+      <Box gray600 leadingSnug>
+        PenX Agent is a proxy between PenX App and quick input client. With PenX
+        Agent, you can input note quickly in PenX chrome extension, PenX CLI,
+        PenX Raycast plugin, PenX Alfred plugin...
+      </Box>
       <Box mt8>
         <Box
           as="img"
           src="http://localhost:65432/images/penx-agent.png"
-          maxW-560
+          maxW-540
           h="auto"
         />
       </Box>
