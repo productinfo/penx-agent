4509ab25e2a5dc597ad16cfe3b9031a7e99276a2
0xzion <0xzion.penx@gmail.com>
2024-01-21 11:22:20 +0800

    feat: support .env

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,19 @@
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Dotenv = require('dotenv-webpack')
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
-];
+  new Dotenv({
+    path:
+      process.env.NODE_ENV === 'production'
+        ? './.env.production'
+        : './.env.development',
+  }),
+]
